78dfefcb09c7884c85a45443d5445ec73d1cfcec
Weber Yan <yanweibo@cloudolp.com>
2017-12-08 17:25:49 +0800

    Add the init code for this project.

---
+++ src/main/java/com/one/piece/common/CommonProjectApplication.java
@@ -0,0 +1,12 @@
+package com.one.piece.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonProjectApplication.class, args);
+	}
+}
